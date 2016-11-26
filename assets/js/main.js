15b71e0ada2b270abcb33139043eae5f8ab86505
mrcoderon <ilovelife4lyfe@yahoo.com>
2016-11-26 13:29:20 -0800

    updated javascript code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 // Create 4 variable and link them to the calculator screen and buttons.
 var viewNum = document.getElementById("view-num");
-var viewNumScreen = viewNum.innerHTML;
 var blank = "";
-var viewNumLastCharDel = viewNum.innerHTML.substring(0, viewNum.innerHTML.length - 1);
+var numButtonsLength = _(".num").length;
+var opButtonsLength = _(".op").length;
+var delCleEntButtonsLength = _(".cl2").length;
+var clearScreen = function() { viewNum.innerHTML += blank; }
 
 function _(id) {
   return document.querySelectorAll(id);
@@ -12,14 +14,14 @@ function _onClick() {
 }
 
 // Loops until there are no more number buttons.
-for (i = 0; i < _(".num").length; i++) {
+for (i = 0; i < numButtonsLength; i++) {
   // During each iteration a NumButton instance is created.
   var numButtons = new NumButton_Constructor(i);
 };
-for (x = 0; x < _(".op").length; x++) {
+for (x = 0; x < opButtonsLength; x++) {
   var opButtons = new OpButton_Constructor(x);
 };
-for (y = 0; y < _(".cl2").length; y++) {
+for (y = 0; y < delCleEntButtonsLength; y++) {
   var delCleEntButtons = new delCleEntButton_Constructor(y);
 };
 
@@ -45,82 +47,52 @@ function OpButton_Constructor(x) {
     var op1 = "*", op2 = "/", op3 = "+", op4 = "-", op5 = "%", op6 = ".";
     var viewNumLastCharDel = viewNum.innerHTML.substring(0, viewNum.innerHTML.length - 1);
     var LastCharOfViewEqualOp = [op1, op2, op3, op4, op5, op6].indexOf(viewNumLastChar) > -1;
+    var deleteThenShow = function(op) { viewNum.innerHTML = viewNumLastCharDel; viewNum.innerHTML += op; }
+    var show = function(op) { viewNum.innerHTML += op; }
     switch(x) {
       case 0: {
-        // If an operator already precede the one that's about to be added.
-        if (LastCharOfViewEqualOp) {
-          // delete it.
-          viewNumScreen = viewNumLastCharDel;
-          // Then add the operator assigned to this button.
-          viewNum.innerHTML += op1;
-        } else {
-          // Otherwise, continue and add the corresponding operator to the calculator screen. 
-            viewNum.innerHTML += op1;
-          }
+        // If an operator already precede the one that's about to be added. Delete it then add the operator. Else continue and 
+        LastCharOfViewEqualOp ? deleteThenShow(op1) : show(op1);
         break;
       }
       case 1: {
-        if (LastCharOfViewEqualOp) { 
-          viewNumScreen = viewNumLastCharDel;
-          viewNum.innerHTML += op2;
-        } else {
-            viewNum.innerHTML += op2;
-          }
+        LastCharOfViewEqualOp ? deleteThenShow(op2) : show(op2);
         break;
       }
       case 2: {
-        if (LastCharOfViewEqualOp) { 
-          viewNumScreen = viewNumLastCharDel;
-          viewNum.innerHTML += op3;
-        } else {
-            viewNum.innerHTML += op3;
-          }
+        LastCharOfViewEqualOp ? deleteThenShow(op3) : show(op3);
         break;
       }
       case 3: {
-        if (LastCharOfViewEqualOp) { 
-          viewNumScreen = viewNumLastCharDel;
-          viewNum.innerHTML += op4;
-        } else {
-            viewNum.innerHTML += op4;
-          }
+        LastCharOfViewEqualOp ? deleteThenShow(op4) : show(op4);
         break;
       }
       case 4: {
-        if (LastCharOfViewEqualOp) { 
-          viewNumScreen = viewNumLastCharDel;
-          viewNum.innerHTML += op5;
-        } else {
-            viewNum.innerHTML += op5;
-          }
+        LastCharOfViewEqualOp ? deleteThenShow(op5) : show(op5);
         break;
       }
       case 5: {
-        if (LastCharOfViewEqualOp) { 
-          viewNumScreen = viewNumLastCharDel;
-          viewNum.innerHTML += op6;
-        } else {
-            viewNum.innerHTML += op6;
-          }
+        LastCharOfViewEqualOp ? deleteThenShow(op6) : show(op6);
         break;
       }
       default: {
-        viewNum.innerHTML += blank
+        clearScreen();
       }
     }
   });
 }
 
 // Delete, Clear, and Enter Button Constructor
-function delCleEntButton_Constructor(y) {
+function delCleEntButton_Constructor(y, c) {
   this.y = y;
   // Wait and listen for a Delete, Clear, or Enter button to be clicked on.
   _(".cl2")[y].addEventListener('click', function () {
     // Vars
+    var viewNumLastCharDel = viewNum.innerHTML.substring(0, viewNum.innerHTML.length - 1);
     switch(y) {
       case 0: {
         // If the Delete button is pressed, remove the last character of the string assigned to the calculator screen.
-        viewNumScreen = viewNumLastCharDel;
+        viewNum.innerHTML = viewNumLastCharDel;
         break;
       }
       case 1: {
@@ -133,6 +105,7 @@ function delCleEntButton_Constructor(y) {
         // If the Enter button is pressed, copy the string of operators and numbers from the calculator screen and assign it to a new variable.
         function copyView() {
           resultnum = viewNum.innerHTML;
+          viewNum.innerHTML = blank;
           clearView();
         }
         // Afterwards, clear the calculator screen by assigning a blank string to the calculator screen.
@@ -142,7 +115,7 @@ function delCleEntButton_Constructor(y) {
         }
         function newView(clearView) {
         // Then check the new variable and see if the evaluation failed.
-          if (resultnum === blank) {
+          if (resultnum === "") {
           // If it failed clear the calculator screen.
             viewNum.innerHTML = blank;
           } else {
@@ -155,9 +128,10 @@ function delCleEntButton_Constructor(y) {
       }
       default:
         // Blank screen.
-        viewNum.innerHTML = blank
+        viewNum.innerHTML = blank;
     }
   });
 }
+
 
 
